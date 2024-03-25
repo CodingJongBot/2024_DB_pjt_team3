f81eb4e0e3974e28bdabcdd59669e5d7201191fe
unknown <oasis9311@gmail.com>
2024-03-25 13:30:11 +0900

    modify query(add rec__num variable)

--- main.py
+++ main.py
@@ -215,12 +215,16 @@ def ibcf(user_input=True, user_id=None, rec_threshold=None, rec_max_cnt=None):
     ]
 
     # item별 유사도 상위 k개
-    query = "SELECT sb.item_1,sb.sim,sb.item_2,sb.sim/SUM(sb.sim) OVER (PARTITION BY item_1) as norm_sim\
+    query = (
+        "SELECT sb.item_1,sb.sim,sb.item_2,sb.sim/SUM(sb.sim) OVER (PARTITION BY item_1) as norm_sim\
             FROM (SELECT item_1,sim,item_2,ROW_NUMBER() OVER (PARTITION BY item_1 ORDER BY sim DESC, item_2 ASC) AS raking\
                 FROM item_similarity) as sb\
-            WHERE sb.raking <=5 #K"
+            WHERE sb.raking <="
+        + rec_num
+    )
     rs1 = get_output(query)
 
+    # update null value its average
     query2 = "UPDATE ratings rt\
             JOIN (\
                 SELECT item, AVG(rating) as avg_rating\
